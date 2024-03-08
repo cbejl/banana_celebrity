b98214f0db591a11f96f7f6b7015a55d8772937b
cbejl <superslavyanin@gmail.com>
2024-03-08 19:41:56 +0300

    hotfix

--- src/main/java/cbejl/banana_celebrity/CelebrityCommand.java
+++ src/main/java/cbejl/banana_celebrity/CelebrityCommand.java
@@ -13,29 +13,31 @@ public class CelebrityCommand implements CommandExecutor {
     public boolean onCommand(@NotNull CommandSender commandSender, @NotNull Command command, @NotNull String s, @NotNull String[] strings) {
         if(commandSender instanceof Player player) {
             if(player.hasPermission("celebrity.do")) {
-                switch (strings[0]) {
-                    case "stream" -> {
-                        Banana_celebrity.getInstance().celebrityMap.put(player,
-                                Banana_celebrity.getInstance().getConfig().getString("papi.stream"));
-                        player.sendMessage(Banana_celebrity.getInstance().getConfig().getString("set.stream"));
-                    }
-                    case "rec" -> {
-                        Banana_celebrity.getInstance().celebrityMap.put(player,
-                                Banana_celebrity.getInstance().getConfig().getString("papi.rec"));
-                        player.sendMessage(Banana_celebrity.getInstance().getConfig().getString("set.rec"));
-                    }
-                    case "justplay" -> {
-                        Banana_celebrity.getInstance().celebrityMap.put(player,
-                                Banana_celebrity.getInstance().getConfig().getString("papi.justplay"));
-                        player.sendMessage(Banana_celebrity.getInstance().getConfig().getString("set.justplay"));
-                    }
-                    default -> {
-                        if(Banana_celebrity.getInstance().celebrityMap.get(player).equals(Banana_celebrity.getInstance().getConfig().getString("papi.stream"))) {
-                            player.sendMessage(Banana_celebrity.getInstance().getConfig().getString("is.stream"));
-                        } else if(Banana_celebrity.getInstance().celebrityMap.get(player).equals(Banana_celebrity.getInstance().getConfig().getString("papi.rec"))) {
-                            player.sendMessage(Banana_celebrity.getInstance().getConfig().getString("is.rec"));
-                        } else if(Banana_celebrity.getInstance().celebrityMap.get(player).equals(Banana_celebrity.getInstance().getConfig().getString("papi.justplay"))) {
-                            player.sendMessage(Banana_celebrity.getInstance().getConfig().getString("is.justplay"));
+                if(strings.length > 0 && !strings[0].isEmpty()) {
+                    switch (strings[0]) {
+                        case "stream" -> {
+                            Banana_celebrity.getInstance().celebrityMap.put(player,
+                                    Banana_celebrity.getInstance().getConfig().getString("papi.stream"));
+                            player.sendMessage(Banana_celebrity.getInstance().getConfig().getString("set.stream"));
+                        }
+                        case "rec" -> {
+                            Banana_celebrity.getInstance().celebrityMap.put(player,
+                                    Banana_celebrity.getInstance().getConfig().getString("papi.rec"));
+                            player.sendMessage(Banana_celebrity.getInstance().getConfig().getString("set.rec"));
+                        }
+                        case "justplay" -> {
+                            Banana_celebrity.getInstance().celebrityMap.put(player,
+                                    Banana_celebrity.getInstance().getConfig().getString("papi.justplay"));
+                            player.sendMessage(Banana_celebrity.getInstance().getConfig().getString("set.justplay"));
+                        }
+                        default -> {
+                            if(Banana_celebrity.getInstance().celebrityMap.get(player).equals(Banana_celebrity.getInstance().getConfig().getString("papi.stream"))) {
+                                player.sendMessage(Banana_celebrity.getInstance().getConfig().getString("is.stream"));
+                            } else if(Banana_celebrity.getInstance().celebrityMap.get(player).equals(Banana_celebrity.getInstance().getConfig().getString("papi.rec"))) {
+                                player.sendMessage(Banana_celebrity.getInstance().getConfig().getString("is.rec"));
+                            } else if(Banana_celebrity.getInstance().celebrityMap.get(player).equals(Banana_celebrity.getInstance().getConfig().getString("papi.justplay"))) {
+                                player.sendMessage(Banana_celebrity.getInstance().getConfig().getString("is.justplay"));
+                            }
                         }
                     }
                 }
